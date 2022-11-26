ace6ba8cf4e5dc33d8a365bd18ce75c2bfb6182a
monja199 <monja.sesame@gmail.com>
2022-11-26 11:12:48 +0300

    updating

--- Peaky_Coders/settings.py
+++ Peaky_Coders/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'Peaky_Coders.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
